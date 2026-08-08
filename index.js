dc8698d7310a9f408d06f5ec387b92709aa1ae8d
pitzrockin141 <pratikradia92@gmail.com>
2026-08-08 16:05:09 +0530

    Add SNI servername and standard headers to proxy fetch to prevent CDN blocks

--- index.js
+++ index.js
@@ -54,8 +54,14 @@ async function fetchWithProxy(url, options = {}) {
       port: parsedUrl.port,
       path: parsedUrl.path,
       method: options.method || 'GET',
-      headers: options.headers || {},
+      headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        ...options.headers
+      },
       agent: proxyAgent,
+      servername: parsedUrl.hostname, // Important for SNI through proxy
       rejectUnauthorized: false
     };
 
